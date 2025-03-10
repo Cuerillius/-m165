ecba6fb94aa3eaa57fcf44dbc45f14d9866c9e07
Battino <battino46@gmail.com>
2025-03-10 10:29:10 +0100

    add metrics application

---
+++ floew/p-02/metrics-app/src/index.ts
@@ -0,0 +1,13 @@
+import express, { type Request, type Response } from "express";
+
+const app = express();
+const port = 3000;
+
+app.get("/metrics", (req: Request, res: Response) => {
+  const randomNumber = Math.floor(Math.random() * 100) + 1;
+  res.send(randomNumber.toString());
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
